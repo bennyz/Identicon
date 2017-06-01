edb3c78d4cf56c29e8f695dfd56d853125216357
Benny Zlotnik <bzlotnik@redhat.com>
2017-06-01 12:02:13 +0300

    Mirroring a row is complete

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,14 +1,37 @@
 defmodule Identicon do
+  @moduledoc """
+    Handles an Identicon
+  """
+
   def main(input) do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail] } = image
-    [r, g, b]   
-end
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+
+  end
+  
+  @doc """
+    Mirrors a row
+
+    ## Examples
+      iex> Identicon.mirror_row([1, 2, 3])
+      [1, 2, 3, 2, 1]
+
+  """
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail] } = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
